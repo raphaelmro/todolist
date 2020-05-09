d4af449ed9a61e376aee6ca668b7916ca31c1b63
Raphael Onofre <raphaelmoura@codata.pb.gov.br>
2020-05-09 02:17:45 -0300

    Create store, actions and mutations

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,27 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    todoList: []
+  },
+  mutations: {
+    INSERT_TASK(state, task) {
+      this.state.todoList.push(task)
+    },
+    COMPLETE_TASK(state, taskId) {
+      this.state.todoList.find(currentTask => {
+        if (currentTask.id === taskId) {
+          currentTask.completed = !currentTask.completed
+        }
+      })
+    }
+  },
+  actions: {
+    insertTask({ commit }, task) {
+      commit('INSERT_TASK', task)
+    },
+    completeTask({ state, commit }, taskId) {
+      commit('COMPLETE_TASK', taskId)
+    }
+  }
 })
