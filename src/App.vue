7c611196fda385507112fd737b0e4e2ae656d3d4
Raphael Onofre <raphaelmoura@codata.pb.gov.br>
2020-05-09 02:18:20 -0300

    Convert local management into vuex

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <input placeholder="Insert a task" v-model="task" type="text" />
+    <label>
+      <input placeholder="Insert a task" v-model="task" type="text" />
+    </label>
     <button @click.prevent="insertTask">Add</button>
     <br />
     <ul v-for="task in todoList" :key="task.id">
@@ -8,32 +10,38 @@
         :class="{ 'completed-task': task.completed }"
         :id="task.id"
         @click="completeTask"
-      >{{ task.description }}</li>
+      >
+        {{ task.description }}
+      </li>
     </ul>
     <span class="errorMessage" v-if="errorMessage">Insert a task!</span>
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   data() {
     return {
-      todoList: [],
       task: '',
       errorMessage: false,
       id: null
     }
   },
+  computed: {
+    ...mapState(['todoList'])
+  },
   methods: {
     insertTask() {
       if (this.task.length !== 0) {
         this.errorMessage = false
-        this.todoList.push({
+        this.$store.dispatch('insertTask', {
           id: this.id + 1,
           description: this.task,
           completed: false
         })
-        this.id++
+        this.id = this.id + 1
         this.task = ''
       } else {
         this.errorMessage = !this.errorMessage
@@ -41,11 +49,7 @@ export default {
     },
     completeTask() {
       const taskId = parseInt(event.target.id)
-      this.todoList.find(currentTask => {
-        if (currentTask.id === taskId) {
-          currentTask.completed = !currentTask.completed
-        }
-      })
+      this.$store.dispatch('completeTask', taskId)
     }
   }
 }
